34e5e9e55a32395c6290b9f98e9f0413f5fa168a
Eduard Correa <eduard.correa@firmwareco.com>
2021-11-12 09:11:13 -0400

    bug en reportes y login

--- src/app/router/helperRoute.jsx
+++ src/app/router/helperRoute.jsx
@@ -11,9 +11,6 @@ const MapaPage = lazy(() =>
 const FondosDeComercioPage = lazy(() =>
   import("../modules/FondoDeComercio/FondoDeComercio")
 );
-const ReporteComprobanteDeInscripcionPage = lazy(() =>
-  import("../modules/Reports/ComprobanteDeInscripcion")
-);
 const CrearFondosDeComercioPage = lazy(() =>
   import("../modules/FondoDeComercio/FondoDeComercioCrear")
 );
@@ -91,13 +88,7 @@ export const PathListContribuyente = [
     component: UserDatosPage
   },
   {
-    path: "/comprobante-de-inscripcion",
-    groups: ["contribuyentes"],
-    name: "Comprobante de inscripción",
-    component: ReporteComprobanteDeInscripcionPage
-  },
-  {
-    path: "/reportes/certificado-solvencia",
+    path: "/reportes/:reporte",
     groups: ["contribuyentes"],
     name: "Certificado de solvencia",
     component: ReportsPage
@@ -275,7 +266,7 @@ export const navContribuyentes = [
     childrens: [
       {
         title: "Comprobante de inscripción",
-        url: "/comprobante-de-inscripcion",
+        url: "/reportes/comprobante-de-inscripcion",
         icon: "",
         slug: "comprobante-de-inscripción",
       },
